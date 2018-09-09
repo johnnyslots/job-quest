e4e1d6da7e825a61e51f52dfec419dd5ef6973f0
Yoni Slotwinere <yonislots@gmail.com>
2018-09-09 16:32:34 -0400

    server listening successfully

--- server/index.js
+++ server/index.js
@@ -2,6 +2,21 @@ const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
 const bodyParser = require('body-parser')
-
+const PORT = process.env.PORT || 3000
 const app = express()
+
+const startListening = () => {
+  const server = app.listen(PORT, () => {
+    console.log(`You are now live on port ${PORT}`)
+  })
+}
+
+app.use('/', (req, res, next) => {
+  console.log('HEYYY')
+  res.json('HEYYY')
+})
+
+startListening()
+
+module.exports = app
 
